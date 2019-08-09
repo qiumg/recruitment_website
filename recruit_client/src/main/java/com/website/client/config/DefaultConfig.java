91234ae3dceee50c892dd790b25ca0ac40d7696a
qmg <qmg@lanqiao.org>
2019-08-09 18:02:09 +0800

    前端页面的添加

---
+++ recruit_client/src/main/java/com/website/client/config/DefaultConfig.java
@@ -0,0 +1,12 @@
+package com.website.client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class DefaultConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+}
