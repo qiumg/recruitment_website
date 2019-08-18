903b8679edc228f7476a1033f5b0f9de3d2ce422
qmg <qmg@lanqiao.org>
2019-08-18 09:03:57 +0800

    前端页面修改

--- recruit_server/src/main/java/com/website/server/controller/IndexController.java
+++ recruit_server/src/main/java/com/website/server/controller/IndexController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("/index")
 public class IndexController {
     @Autowired
     private IJobKindInfoService jobKindInfoService;
